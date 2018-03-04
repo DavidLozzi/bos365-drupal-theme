c842267cf055f565432b101fb401482243227308
David Lozzi <david.lozzi@slalom.com>
2018-03-04 17:03:54 -0500

    early work

---
+++ bos365/config/_baseConfig.js
@@ -0,0 +1,13 @@
+/*
+USE this template to create your config for build.
+Copy this file and rename to yours.config.js, and git will ignore it
+*/
+
+module.exports = {
+    FTP_CONNECTION: {
+            host: 'the.server.name.com',
+            user: 'username',
+            pass: 'p@ssw0rd'
+        },
+    FTP_PATH: '/path/to/root/of/theme/folder'
+}
--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,64 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var notify = require("gulp-notify");
 var runSequence = require('run-sequence');
+var minimist = require('minimist');
+var ftp = require('vinyl-ftp');
+
+var options = minimist(process.argv.slice(2), {
+    string: ['config'],
+    boolean: ['minify', 'deploy', 'watch'],
+    default: {
+      config: 'bos365',
+      minify: false,
+      deploy: false,
+      watch: false
+    }
+  });
+
+var config = require('./bos365/config/' + options.config + '.config.js');
 
 gulp.task('default', function() {
-    runSequence('css');
+    if(options.deploy){
+        runSequence('css','cssFtp','templatesFtp');
+    }else if(options.watch){
+        runSequence('css',['cssFtp','templatesFtp'],'watchers');
+    }else{
+        runSequence('css');
+    }
 });
 
 gulp.task('css', compileCss);
+gulp.task('watchers', watchers);
+gulp.task('cssFtp', ftpCss);
+gulp.task('templatesWatch', watchers);
+gulp.task('templatesFtp', ftpTemplates);
 
 function compileCss(){
     return gulp.src('bos365/scss/style.scss')
-        .pipe(sass().on("error", notify.onError(function (error) {
+        .pipe(sass()
+            .on("error", notify.onError(function (error) {
                 return "Error: " + error.message;
             })))
-        .pipe(gulp.dest('./bos365/css'))
+        .pipe(gulp.dest('./bos365/css'));
+}
+
+function ftpCss(){
+    var conn = ftp.create(config.FTP_CONNECTION);
+
+    var globs = ['bos365/css/style.css'];
+    return gulp.src(globs, { base: '.', buffer: false})
+    .pipe(conn.dest(config.FTP_PATH));
+}
+
+function watchers(){
+    gulp.watch('./bos365/templates/**/*', ['templatesFtp']);
+    gulp.watch('./bos365/scss/**/*.scss', ['css','cssFtp']);
+}
+
+function ftpTemplates(){
+    var conn = ftp.create(config.FTP_CONNECTION);
+
+    var globs = ['bos365/templates/**/*'];
+    return gulp.src(globs, { base: '.', buffer: false})
+    .pipe(conn.dest(config.FTP_PATH));
 }
